4755713806c327afbe0bebcca12ba10539ff8f15
Patrick Indran <>
2026-05-03 07:50:40 +0200

    added Plymouth, and kernalparms for silent and "sexy" boot

--- nix/modules/system/bootloader.nix
+++ nix/modules/system/bootloader.nix
@@ -5,4 +5,28 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # --- Boot --- 
+
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+  ];
+
+  # --- plymouth ---
+
+  boot.plymouth = {
+    enable = true;
+    theme = "bgrt"; 
+  };
+
+
+
 }
