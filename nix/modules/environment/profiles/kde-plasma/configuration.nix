865cb291b772be7331d11514daf734681189de6b
Patrick Indran <>
2026-05-03 13:52:26 +0200

    pray it works!

--- nix/modules/environment/profiles/kde-plasma/configuration.nix
+++ nix/modules/environment/profiles/kde-plasma/configuration.nix
@@ -1,16 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  environment.etc."ventoo/assets/wallpapers/glaze_1.heic".source = 
-    ../../../../../src/themes/kde-plasma/ventoo.dark.desktop/wallpapers/glaze_1.heic;
-
   environment.systemPackages = [
+    (pkgs.stdenv.mkDerivation {
+      name = "ventoo-wallpapers";
+      src = ../../../../../src/themes/kde-plasma/ventoo.dark.desktop/wallpapers;
+      installPhase = ''
+        cp -r . $out/share/wallpapers/
+      '';
+    })
+
     (pkgs.stdenv.mkDerivation {
       name = "ventoo-plasma-theme";
       src = ../../../../../src/themes/kde-plasma/ventoo.dark.desktop/look-and-feel;
       installPhase = ''
-        mkdir -p $out/share/plasma/look-and-feel
-        cp -r VentooThemeDark $out/share/plasma/look-and-feel/org.ventoo.dark.desktop
+        cp -r org.ventoo.dark.desktop $out/share/plasma/look-and-feel/org.ventoo.dark.desktop
       '';
     })
   ];
@@ -19,5 +23,4 @@
     [KDE]
     LookAndFeelPackage=org.ventoo.dark.desktop
   '';
-
 }
