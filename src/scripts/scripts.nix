fcaa5ccf022a2b8bab69128ab28691dc1f093bcf
Patrick Indran <>
2026-05-06 21:19:52 +0200

    disabled doCheck

--- src/scripts/scripts.nix
+++ src/scripts/scripts.nix
@@ -9,7 +9,8 @@ softwareupdate = pkgs.writers.writePython3Bin "softwareupdate"
     libraries = with pkgs.python3Packages; [
       requests 
       pygobject3 
-    ]; 
+    ];
+    doCheck = false;
   } 
   (builtins.readFile ./vento-softwareupdate/softwareupdate.py);
 
