863866ba4a90d3e0fc6d15cbe0eb174831ee7243
Patrick Indran <>
2026-05-06 21:39:47 +0200

    okay python keeps crying

--- src/scripts/scripts.nix
+++ src/scripts/scripts.nix
@@ -12,10 +12,6 @@ softwareupdate = pkgs.writers.writePython3Bin "softwareupdate"
       pkgs.libnotify
       pkgs.gobject-introspection
     ];
-
-    makeWrapperArgs = [
-      "--prefix GI_TYPELIB_PATH : ${pkgs.libnotify}/lib/girepository-1.0:${pkgs.gobject_introspection}/lib/girepository-1.0"
-    ];
     doCheck = false;
   } 
   (builtins.readFile ./vento-softwareupdate/softwareupdate.py);
