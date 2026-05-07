4c229f62e639643ae46e82515f877eaab643d1ae
Patrick Indran <>
2026-05-07 21:27:32 +0200

    added theme to packages

--- src/packages.nix
+++ src/packages.nix
@@ -16,5 +16,19 @@ softwareupdate = pkgs.writers.writePython3Bin "softwareupdate"
   } 
   (builtins.readFile ./scripts/vento-softwareupdate/softwareupdate.py);
 
+  vento-theme = pkgs.stdenv.mkDerivation {
+    name = "vento-dark-theme";
+    
+    src = ./themes/kde-plasma/org.vento.dark/; 
+
+    installPhase = ''
+      mkdir -p $out/share/plasma/look-and-feel/org.vento.dark
+      cp -r ./* $out/share/plasma/look-and-feel/org.vento.dark/
+      
+      mkdir -p $out/share/icons/hicolor/scalable/apps
+      cp ./icons/vento_logomart.svg $out/share/icons/hicolor/scalable/apps/vento_logomart.svg
+    '';
+  };
+
 }  
   
