d95b2eced0d685a9e54161a475994cc0969a8853
Patrick Indran <>
2026-05-07 21:25:34 +0200

    removed the icon from taskbar which didnt even work in the first place

--- nix/modules/environment/profiles/kde-plasma/configuration.nix
+++ nix/modules/environment/profiles/kde-plasma/configuration.nix
@@ -11,9 +11,5 @@
     ColorScheme=BreezeDark
   '';
 
-  environment.etc."xdg/plasma-org.kde.plasma.desktop-appletsrc".text = ''
-    [Serialization][Industrial][Applets][2][Configuration][General]
-    icon=${../../../../../src/branding/logo_designs/icons/vento_logomart/vento_logomart.svg}
-  '';
 
 }
