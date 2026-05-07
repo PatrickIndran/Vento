0863aa28de2440e29ccadc7dfdd4341bb7872eea
Patrick Indran <>
2026-05-07 21:21:29 +0200

    more fixes for some odd reason

--- flake.nix
+++ flake.nix
@@ -8,62 +8,47 @@
   outputs = { self, nixpkgs, ... }@inputs: 
   let
     system = "x86_64-linux";
-
     pkgs = nixpkgs.legacyPackages.${system};
     vento-packages = import ./src/packages.nix { inherit pkgs; };
   in {
     
-   
     nixosModules = {
-      
-      # --- Main ---
-
       main = {
         imports = [
           ./nix/modules/system/system.nix
           ./nix/modules/environment/environment.nix
           ./nix/modules/environment/profiles/kde-plasma/default.nix
-
           ({ ... }: {
             system.configurationRevision = self.rev or "dirty";
           })
-
         ];
       };
       
-      # --- Sandbox ---    
-
       sandbox = {
         imports = [
           ./hosts/sandbox/sandbox.nix    
           ./nix/modules/system/system.nix
           ./nix/modules/environment/environment.nix
           ./nix/modules/environment/profiles/kde-plasma/default.nix
-
-          
           ({ ... }: {
             system.configurationRevision = self.rev or "dirty";
           })
-
-
         ];
       };
     };
 
-    nixosconfigurations = {
-      
-      main = nixpkgs.lib.nixossystem {
+    nixosConfigurations = {
+      main = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [ self.nixosmodules.main ];
-        specialargs = { inherit vento-packages; };
+        specialArgs = { inherit vento-packages; };
+        modules = [ self.nixosModules.main ];
       };
 
-      sandbox = nixpkgs.lib.nixossystem {
+      sandbox = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [ self.nixosmodules.sandbox ];
-        specialargs = { inherit vento-packages; };
+        specialArgs = { inherit vento-packages; };
+        modules = [ self.nixosModules.sandbox ];
       };
-      
     };
   };
 }
