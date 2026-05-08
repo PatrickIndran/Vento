e4b6780983db40850fdad242c0ebca0b21b76f89
Patrick Indran <>
2026-05-08 17:18:19 +0200

    updated packages.nix in src

--- src/packages.nix
+++ src/packages.nix
@@ -1,21 +1,27 @@
 { pkgs }:
 
 {
-
   # --- vento-softwareupdate ---
+  softwareupdate = pkgs.writers.writePython3Bin "softwareupdate" {
+    libraries = with pkgs.python3Packages; [
+      requests
+      pygobject3
+    ];
 
-  #softwareupdate = pkgs.writers.writePython3Bin "softwareupdate"
-  #  {
-  #    libraries = with pkgs.python3Packages; [
-  #      requests
-  #      pygobject3
-  #      pkgs.libnotify
-  #      pkgs.gobject-introspection
-  #    ];
-  #    doCheck = false;
-  #  }
-  #  (builtins.readFile ./scripts/vento-softwareupdate/softwareupdate.py);
-  #
+    makeWrapperArgs = [
+      "--prefix"
+      "GI_TYPELIB_PATH"
+      ":"
+      "${pkgs.libnotify}/lib/girepository-1.0"
+      "--prefix"
+      "PATH"
+      ":"
+      "${pkgs.libnotify}/bin"
+    ];
+
+    doCheck = false;
+  } (builtins.readFile ./scripts/vento-softwareupdate/softwareupdate.py);
+
   #  vento-theme = pkgs.stdenv.mkDerivation {
   #    name = "vento-dark-theme";
   #
